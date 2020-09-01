f0676524d4ac5ace5dc4d2785184f1c1a85975ff
João Paulo Lima <jptick@gmail.com>
2020-09-01 19:52:56 -0300

    Config Routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
-import express from 'express';
+import express, { response } from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users',(request, response) =>{
-  console.log('Listagem de usuários');
-  response.json([
-    'JP'
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333)
